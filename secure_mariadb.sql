91d2c72ef531cc562772692646adbf0a329802bb
Siddharth Deshpande <siddharth.deshpande@gmail.com>
2019-06-28 02:22:40 -0400

    fixing mysql password error

--- secure_mariadb.sql
+++ secure_mariadb.sql
@@ -1,3 +1,4 @@
+SET old_passwords = 0;
 SET PASSWORD FOR root@localhost = PASSWORD('P@$$w0rd');
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
